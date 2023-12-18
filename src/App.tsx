7543bd2ecb03058243d76b36f0756d93dca4afab
aradhya-gupta <aradhya.gupta0308@gmail.com>
2023-12-18 13:46:22 +0530

    Build game card

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,7 @@ function App() {
           Aside
         </GridItem>
       </Show>
-      <GridItem area="main" bg="teal">
+      <GridItem area="main">
         <GameGrid />
       </GridItem>
     </Grid>
