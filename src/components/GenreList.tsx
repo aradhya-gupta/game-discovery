92d13ace3215eb6836664961349aed1a76b1e8ed
aradhya-gupta <aradhya.gupta0308@gmail.com>
2023-12-21 09:09:05 +0530

    Show the genres in side panel

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,6 +1,23 @@
+import { HStack, Image, List, ListItem, Text } from "@chakra-ui/react";
 import useGenres from "../hooks/useGenres";
+import getCroppedImageUrl from "../services/image-url";
 
 export const GenreList = () => {
   const { genres, isLoading, error } = useGenres();
-  return <>{genres.map((genre) => genre.name)}</>;
+  return (
+    <List>
+      {genres.map((genre) => (
+        <ListItem key={genre.id} paddingY="5px">
+          <HStack>
+            <Image
+              boxSize="32px"
+              borderRadius={8}
+              src={getCroppedImageUrl(genre.image_background)}
+            />
+            <Text fontSize="lg">{genre.name}</Text>
+          </HStack>
+        </ListItem>
+      ))}
+    </List>
+  );
 };
