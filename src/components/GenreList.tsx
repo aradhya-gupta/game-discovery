ec20d140b8387d29d096f008765e85cfc9ee8e5d
aradhya-gupta <aradhya.gupta0308@gmail.com>
2023-12-21 10:26:31 +0530

    Show skeleton for sidebar genres list

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,9 +1,26 @@
-import { HStack, Image, List, ListItem, Text } from "@chakra-ui/react";
+import {
+  HStack,
+  Image,
+  List,
+  ListItem,
+  Skeleton,
+  Stack,
+  Text,
+} from "@chakra-ui/react";
 import useGenres from "../hooks/useGenres";
 import getCroppedImageUrl from "../services/image-url";
 
 export const GenreList = () => {
   const { genres, isLoading, error } = useGenres();
+  const genresSkeletons = [1, 2, 3, 4, 5, 6, 6, 6, 6, 6, 6, 6, 6, 6];
+  if (isLoading)
+    return (
+      <Stack>
+        {genresSkeletons.map((g) => (
+          <Skeleton height="42px" marginY="5px" />
+        ))}
+      </Stack>
+    );
   return (
     <List>
       {genres.map((genre) => (
